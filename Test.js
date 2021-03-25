9c83685d1899681a03d46d34aece602e9edf1920
Andy Edwards <jedwards@fastmail.com>
2021-03-24 21:57:04 -0500

    feat: initial commit

---
+++ Test.js
@@ -0,0 +1,12 @@
+/**
+ * @flow
+ * @prettier
+ */
+
+import * as React from 'react'
+
+export default class Comp extends React.Component<{}, {}> {
+  render(): React.Node | null {
+    return null
+  }
+}
